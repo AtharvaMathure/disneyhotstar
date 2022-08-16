767f8e95a68f558edfd1986b20b2c9808ca5cfca
AtharvaMathure <atharvamathure@gmail.com>
2022-08-16 19:05:04 +0530

    new update

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies = [
+    {
+        name: 'Falcon and the Winter Soldier',
+        des: 'Based on Marvel Comics featuring the characters Sam Wilson / Falcon and Bucky Barnes / Winter Soldier. It is the second television series in the Marvel Cinematic Universe (MCU) produced by Marvel Studios, sharing continuity with the films of the franchise and taking place after the events of the film Avengers: Endgame (2019). Spellman served as head writer with Kari Skogland directing.',
+        image: 'images/slider 2.png' 
+    },
+    {
+        name: 'Loki',
+        des: 'Based on Marvel Comics featuring the character of the same name. It is the third television series in the Marvel Cinematic Universe (MCU) produced by Marvel Studios, sharing continuity with the films of the franchise. The series takes place after the events of the film Avengers: Endgame (2019), in which an alternate version of Loki created a new timeline. Waldron serves as head writer, with Kate Herron directing for the first season.',
+        image: 'images/slider 1.png'   
+    },
+    {
+        
+        name: 'Wanda Vision',
+        des: 'Based on Marvel Comics featuring the characters Wanda Maximoff / Scarlet Witch and Vision. It is the first television series in the Marvel Cinematic Universe (MCU) produced by Marvel Studios, sharing continuity with the films of the franchise and taking place after the events of the film Avengers: Endgame (2019). Schaeffer served as head writer with Matt Shakman directing.',
+        image: 'images/slider 3.png'
+    },
+    {
+        name: 'Raya and the Last Dragon',
+        des:'A 2021 American computer-animated fantasy action-adventure film that was produced by Walt Disney Animation Studios and directed by Don Hall and Carlos López Estrada. The screenplay was written by Qui Nguyen and Adele Lim, and is based on traditional Southeast Asian cultures. The film stars the voices of Kelly Marie Tran, Awkwafina, Izaac Wang, Gemma Chan, Daniel Dae Kim, Benedict Wong, and Sandra Oh. The plot follows a warrior princess who searches for the fabled last dragon, hoping to restore the dragon gem that would banish the evil spirits known as the Druun from the land of Kumandra',
+        image: 'images/slider 4.png'
+    },
+    {
+        name: 'Luca',
+        des: 'A 2021 American computer-animated coming-of-age fantasy film produced by Pixar Animation Studios and distributed by Walt Disney Studios Motion Pictures. The film was directed by Enrico Casarosa (in his feature-length directorial debut), written by Jesse Andrews and Mike Jones, from a story by Casarosa, Andrews, and Simon Stephenson, produced by Andrea Warren, and starring the voices of Jacob Tremblay, Jack Dylan Grazer, Emma Berman, Saverio Raimondo, Marco Barricelli, Maya Rudolph, Jim Gaffigan, Peter Sohn, Lorenzo Crisci, Marina Massironi, and Sandy Martin.',
+        image: 'images/slider 5.png'
+    }
+]
